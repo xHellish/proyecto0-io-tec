6b6554d1a439e4d3dbc0ddfa3164e223f136ffb1
xHellish <benfarz.bf25@gmail.com>
2026-09-10 10:41:24 -0600

    Avance en funciones para obtener los datos (función Z + Restricciones) | Nueva estructura Restriccion

--- aux.h
+++ aux.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #define MAX_VARS 20 // Límite de variables por enunciado
+#define MAX_RESTRICCIONES 20 // Límite de restricciones
 
 // --------------------------------- //
 // Structs
@@ -13,10 +14,19 @@ typedef struct {
     int id_var; // x1 , x2, x3, ...
 } Variable;
 
+typedef struct {
+    Variable variables[MAX_VARS];
+    int num_vars;
+    char tipo_restriccion[5];  // "<=", ">=", "="
+    int termino_independiente;  // Lo que está a la derecha del operador de restricción
+} Restriccion;
+
 // --------------------------------- //
 // Funciones
 void pedir_datos(void);
 
-int extraer_coeficientes(const char *cadena, Variable variables[], int *num_vars);
+int extraer_funcion_z(const char *cadena, Variable variables[], int *num_vars);
+
+int extraer_restriccion(const char *cadena, Restriccion *restriccion);
 
 #endif
